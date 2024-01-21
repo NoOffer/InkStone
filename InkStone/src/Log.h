6aed175dc5d5ea4bef07bf704ba643726da02824
Nofer <sxx261@case.edu>
2024-01-20 23:53:16 -0500

    Added Logging System

---
+++ InkStone/src/Log.h
@@ -0,0 +1,53 @@
+#pragma once
+
+namespace NXTN {
+	class Log
+	{
+	public:
+		template <typename... Args>
+		static void Info(const char* msg, Args... args)
+		{
+			time_t tt = time(0);
+			tm ltm;
+			localtime_s(&ltm, &tt);
+
+			mtx.lock();
+			printf("%02d:%02d:%02d [Info]  ", ltm.tm_hour, ltm.tm_min, ltm.tm_sec);
+			printf(msg, args...);
+			printf("\n");
+			mtx.unlock();
+		}
+
+		template <typename... Args>
+		static void Warning(const char* msg, Args... args)
+		{
+			time_t tt = time(0);
+			tm ltm;
+			localtime_s(&ltm, &tt);
+
+			mtx.try_lock();  // Non-blocking
+			printf("%02d:%02d:%02d [Warn]  ", ltm.tm_hour, ltm.tm_min, ltm.tm_sec);
+			printf(msg, args...);
+			printf("\n");
+			mtx.unlock();
+		}
+
+		template <typename... Args>
+		static void Error(const char* msg, Args... args)
+		{
+			time_t tt = time(0);
+			tm ltm;
+			localtime_s(&ltm, &tt);
+
+			mtx.lock();
+			printf("%02d:%02d:%02d [Error] ", ltm.tm_hour, ltm.tm_min, ltm.tm_sec);
+			printf(msg, args...);
+			printf("\n");
+			mtx.unlock();
+		}
+
+	private:
+		static std::mutex mtx;
+	};
+}
+
