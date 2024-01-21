f951ec52fe144b2beb9800b16a2f4030365dffab
Nofer <sxx261@case.edu>
2024-01-21 00:03:58 -0500

    Added ERROR Macro

--- InkStone/src/Core.h
+++ InkStone/src/Core.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// NXTN_API (__declspec)
 #ifdef NXTN_PLATFORM_WINDOWS
 #ifdef NXTN_BUILD_DLL
 #define NXTN_API __declspec(dllexport)
@@ -9,5 +10,13 @@
 #else
 #error Platform Not Supported
 #endif // NXTN_PLATFORM_WINDOWS
+
+// Error
+#ifdef NXTN_DEBUG
+#define NXTN_ERROR __debugbreak()
+#else
+#define NXTN_ERROR exit(-1)
+#endif // NXTN_DEBUG
+
 
 #define BITMASK(x) (1 << x)
