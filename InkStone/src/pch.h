7c40d596120365bdedb4cbd33e9a4573f9229301
Nofer <sxx261@case.edu>
2024-01-20 23:53:32 -0500

    Added PCH

---
+++ InkStone/src/pch.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifdef NXTN_PLATFORM_WINDOWS
+#include <Windows.h>
+#endif // NXTN_PLATFORM_WINDOWS
+
+// Utility
+#include <algorithm>
+#include <cstdio>
+#include <ctime>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <sstream>
+#include <utility>
+
+// Data structure
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+// Custom includes
+#include "Core.h"
+#include "Log.h"
