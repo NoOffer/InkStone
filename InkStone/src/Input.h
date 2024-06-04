93b86cbf3d8e0f700ad24dbe9b5ecca9060f74c2
Nofer <sxx261@case.edu>
2024-06-04 22:14:42 +0800

    Create Input.h

---
+++ InkStone/src/Input.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Core.h"
+
+namespace NXTN {
+	class NXTN_API Input
+	{
+	public:
+		static bool IsKeyPressed(int keycode)
+		{
+			return s_Instance->IsKeyPressedImpl(keycode);
+		}
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) = 0;
+	private:
+		static Input* s_Instance;
+	};
+}
+
