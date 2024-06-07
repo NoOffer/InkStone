c8fdc8dcdee2f6fbc48dae86fd3b6b5c38b9545e
Nofer <sxx261@case.edu>
2024-06-07 22:16:23 +0800

    Updated WindowsInput (Unfinished)

---
+++ InkStone/src/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+#include "WindowsInput.h"
+
+namespace NXTN {
+
+	// TODO: Implement this module
+
+	Input* Input::s_Instance = nullptr;
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		return false;
+	}
+}
---
+++ InkStone/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Input.h"
+
+#include <GLFW/glfw3.h>
+
+namespace NXTN {
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+	};
+}
