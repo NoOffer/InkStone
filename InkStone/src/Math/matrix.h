a9c1871f1be037cd1140bcbdc2c9680b06d9dc74
Nofer <sxx261@case.edu>
2024-07-29 21:48:22 +0800

    Updated Vector & Matrix

--- InkStone/src/Math/matrix.h
+++ InkStone/src/Math/matrix.h
@@ -1,66 +1,68 @@
 #pragma once
 
-#include "vector.h"
+#include "Vector.h"
 
-struct mat4
-{
-	// Top-left: [0, 0], top-right: [0, 3]
-	float m_Mat[4][4];
-
-	mat4();
-
-	mat4(float a);
-
-	mat4(
-		float m00, float m01, float m02, float m03,
-		float m10, float m11, float m12, float m13,
-		float m20, float m21, float m22, float m23,
-		float m30, float m31, float m32, float m33
-	);
-
-	inline float* operator[](int i)
+namespace NXTN {
+	struct mat4
 	{
-		return &(m_Mat[i][0]);
-	}
+		// Top-left: [0, 0], top-right: [0, 3]
+		float m_Mat[4][4];
 
-	mat4 Transpose();
-};
+		mat4();
 
-struct mat3
-{
-	// Top-left: [0, 0], top-right: [0, 3]
-	float m_Mat[3][3];
+		mat4(float a);
 
-	mat3();
+		mat4(
+			float m00, float m01, float m02, float m03,
+			float m10, float m11, float m12, float m13,
+			float m20, float m21, float m22, float m23,
+			float m30, float m31, float m32, float m33
+		);
 
-	mat3(float a);
+		inline float* operator[](int i)
+		{
+			return &(m_Mat[i][0]);
+		}
 
-	//mat3(
-	//	float m00, float m01, float m02,
-	//	float m10, float m11, float m12,
-	//	float m20, float m21, float m22
-	//);
+		mat4 Transpose();
+	};
 
-	mat3(mat4 m);
-
-	inline float* operator[](int i)
+	struct mat3
 	{
-		return &(m_Mat[i][0]);
-	}
+		// Top-left: [0, 0], top-right: [0, 3]
+		float m_Mat[3][3];
 
-	//mat4 Transpose();
-};
+		mat3();
 
-mat4 Transpose(mat4 m);
+		mat3(float a);
 
-mat4 TranslateMat(vec3 translation);
-mat4 TranslateMat(mat4 m, vec3 translation);
-mat4 TranslateMat(vec4 translation);
-mat4 TranslateMat(mat4 m, vec4 translation);
-mat4 RotateMat(vec3 angles);
-mat4 ScaleMat(vec3 scales);
+		//mat3(
+		//	float m00, float m01, float m02,
+		//	float m10, float m11, float m12,
+		//	float m20, float m21, float m22
+		//);
 
-vec4 mul(mat4 mat, vec4 vec);
-mat4 mul(mat4 m1, mat4 m2);
+		mat3(mat4 m);
 
-std::ostream& operator<<(std::ostream& ostr, mat4& m);
+		inline float* operator[](int i)
+		{
+			return &(m_Mat[i][0]);
+		}
+
+		//mat4 Transpose();
+	};
+
+	mat4 Transpose(mat4 m);
+
+	mat4 TranslateMat(vec3 translation);
+	mat4 TranslateMat(mat4 m, vec3 translation);
+	mat4 TranslateMat(vec4 translation);
+	mat4 TranslateMat(mat4 m, vec4 translation);
+	mat4 RotateMat(vec3 angles);
+	mat4 ScaleMat(vec3 scales);
+
+	vec4 mul(mat4 mat, vec4 vec);
+	mat4 mul(mat4 m1, mat4 m2);
+
+	std::ostream& operator<<(std::ostream& ostr, mat4& m);
+}
--- InkStone/src/Math/vector.cpp
+++ InkStone/src/Math/vector.cpp
@@ -1,68 +1,115 @@
 #include "pch.h"
 
-#include "vector.h"
+#include "Vector.h"
 
-float length(vec2 v)
-{
-	return std::sqrt(v.x * v.x + v.y * v.y);
-}
-
-float length(vec3 v)
-{
-	return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-
-float length(vec4 v)
-{
-	return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w);
-}
-
-vec2 normalize(vec2 v)
-{
-	return v / length(v);
-}
-
-vec3 normalize(vec3 v)
-{
-	float l = length(v);
-	if (l)
+namespace NXTN {
+	float length(const vec2& v)
 	{
-		return v / l;
+		return std::sqrt(v.x * v.x + v.y * v.y);
 	}
-	return vec3();
-}
 
-vec4 normalize(vec4 v)
-{
-	float l = length(v);
-	if (l)
+	float length(const vec3& v)
 	{
-		return v / l;
+		return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 	}
-	return vec4();
-}
 
-float dot(vec2 v1, vec2 v2)
-{
-	return v1.x * v2.x + v1.y * v2.y;
-}
+	float length(const vec4& v)
+	{
+		return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w);
+	}
 
-float dot(vec3 v1, vec3 v2)
-{
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
+	vec2 normalize(vec2 v)
+	{
+		float l = length(v);
+		if (l)
+		{
+			return v / l;
+		}
+		return vec2();
+	}
 
-float dot(vec4 v1, vec4 v2)
-{
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w;
-}
+	vec3 normalize(vec3 v)
+	{
+		float l = length(v);
+		if (l)
+		{
+			return v / l;
+		}
+		return vec3();
+	}
 
-float cross(vec2 v1, vec2 v2)
-{
-	return v1.x * v2.y - v1.y * v2.x;
-}
+	vec4 normalize(vec4 v)
+	{
+		float l = length(v);
+		if (l)
+		{
+			return v / l;
+		}
+		return vec4();
+	}
 
-vec3 cross(vec3 v1, vec3 v2)
-{
-	return vec3{ v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
+	float dot(const vec2& v1, const vec2& v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	float dot(const vec3& v1, const vec3& v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+	}
+
+	float dot(const vec4& v1, const vec4& v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z + v1.w * v2.w;
+	}
+
+	float cross(const vec2& v1, const vec2& v2)
+	{
+		return v1.x * v2.y - v1.y * v2.x;
+	}
+
+	vec3 cross(const vec3& v1, const vec3& v2)
+	{
+		return vec3{ v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x };
+	}
+
+	std::string vec2::ToString()
+	{
+		std::stringstream ss;
+		ss << "(" << x << ", " << y << ")";
+		const std::string s = ss.str();
+		return s;
+	}
+
+	std::string vec2i::ToString()
+	{
+		std::stringstream ss;
+		ss << "(" << x << ", " << y << ")";
+		const std::string s = ss.str();
+		return s;
+	}
+
+	std::string vec3::ToString()
+	{
+		std::stringstream ss;
+		ss << "(" << x << ", " << y << ", " << z << ")";
+		const std::string s = ss.str();
+		return s;
+	}
+
+	std::string vec3i::ToString()
+	{
+		std::stringstream ss;
+		ss << "(" << x << ", " << y << ", " << z << ")";
+		const std::string s = ss.str();
+		return s;
+	}
+
+	std::string vec4::ToString()
+	{
+		std::stringstream ss;
+		ss << "(" << x << ", " << y << ", " << z << ", " << w << ")";
+		const std::string s = ss.str();
+		return s;
+	}
 }
